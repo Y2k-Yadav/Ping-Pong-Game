006d2dd4d09823f05d6a18d33e5f44ea2840aaa8
YadavDevCraft <63380531+Y2k-Yadav@users.noreply.github.com>
2023-12-31 17:29:14 +0530

    Main ( final version )
    in the version all 3 modules are imported paddle scoreboard ball this is the final version of the main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 turtle = Turtle()
 screen = Screen()
@@ -11,14 +14,40 @@ screen.tracer(0)
 right_paddle = Paddle((350,0))
 # right_paddle = Paddle((350,0))  using tuple making it positional argument goto(x,y)
 left_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(right_paddle.up,"Up")
 screen.onkey(right_paddle.down, "Down")
 screen.onkey(left_paddle.up, "w")
 screen.onkey(left_paddle.down, "s")
 game_is_on = True
+
 while game_is_on:
+    # if we use time class it is importent to use
+    # otherwise we dont use time method we can update therough while loop moving 1 pixel
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    #detect the wall to the ball
+    if ball.ycor() > 280  or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect ball colled with paddle and edge of the paddle
+    if  ball.distance(right_paddle) < 60 and ball.xcor() > 320  or ball.distance(left_paddle) < 60 and ball.xcor() < -320 :
+        ball.bounce_x()
+
+
+    #Detect ball will miss the paddle and change the direction
+    if ball.xcor() >  380:
+        ball.reset_position()
+        scoreboard.updatescore_left()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.updatescore_right()
+
 
 screen.exitonclick()
 
